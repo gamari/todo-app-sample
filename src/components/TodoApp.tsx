9a629e0536bf86ba796154aaa7eaefd333a953ce
gamari <gamari4552@gmail.com>
2024-03-09 16:02:49 +0900

    TODO削除機能の実装

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -13,8 +13,15 @@ export const TodoApp = () => {
   const [title, setTitle] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleOnClick = () => {
-    const newTodos: Todo[] = [...todos, { title: "test1", completed: false }];
+  const addTodo = () => {
+    const newTodos: Todo[] = [...todos, { title: title, completed: false }];
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index: number) => {
+    const newTodos: Todo[] = todos.filter((todo, i) => {
+      return i !== index;
+    });
     setTodos(newTodos);
   };
 
@@ -26,15 +33,37 @@ export const TodoApp = () => {
           type="text"
           placeholder="TODOを入力"
           className="border outline-none p-2 rounded-md"
+          onChange={(e) => {
+            const value = e.target.value;
+            setTitle(value);
+          }}
         />
-        <button className="p-2 bg-teal-500 text-white" onClick={handleOnClick}>
+        <button className="p-2 bg-teal-500 text-white" onClick={addTodo}>
           追加
         </button>
       </div>
 
-      <div>
+      <div className="mt-3">
         {todos.map((todo, index) => (
-          <div>{todo.title}</div>
+          <div
+            key={index}
+            className="flex flex-row items-center justify-between"
+          >
+            <div>
+              <input type="checkbox" name="" id="" />
+            </div>
+            <div>{todo.title}</div>
+            <div>
+              <button
+                className="bg-red-500 text-white p-2"
+                onClick={() => {
+                  deleteTodo(index);
+                }}
+              >
+                削除
+              </button>
+            </div>
+          </div>
         ))}
       </div>
       <div>{/* 検索欄 */}</div>
